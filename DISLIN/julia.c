5940856eea18502112d58acb263f417f2f146eaf
Kai <73109076+stackedflows@users.noreply.github.com>
2021-02-21 21:46:47 +0000

    Update julia.c

--- DISLIN/julia.c
+++ DISLIN/julia.c
@@ -22,14 +22,13 @@ long double _abs(long double x, long double y){
 }
 
 int julia_element(long double x, long double y){
-
   long double * julia_element_x = malloc(J_STEPS * sizeof(long double));
   long double * julia_element_y = malloc(J_STEPS * sizeof(long double));
   julia_element_x[0] = x;
   julia_element_y[0] = y;
 
-  int count = 0;
-  for(int i = 0; i < J_STEPS; i++){
+  int iteration_count = 0;
+  for(int i = 1; i < J_STEPS; i++){
     if(_abs(julia_element_x[i], julia_element_y[i]) > r_lim[0]){
       break;
     }
@@ -39,15 +38,18 @@ int julia_element(long double x, long double y){
     else{
       long double next_x = julia_x(julia_element_x[i], julia_element_y[i]);
       long double next_y = julia_y(julia_element_x[i], julia_element_y[i]);
-      julia_element_x[i + 1] = next_x + const_x;
-      julia_element_y[i + 1] = next_y + const_y;
+      julia_element_x[i] = next_x + const_x;
+      julia_element_y[i] = next_y + const_y;
+      iteration_count++;
     }
-    count = i;
   }
-  return count;
+  free(julia_element_x);
+  free(julia_element_y);
+  return iteration_count;
 }
 
 int main(void){
+
   //constants
   const_x = -0.8;
   const_y = 0.156;
@@ -57,46 +59,45 @@ int main(void){
   r_lim[1] = -0.5320213637808087;
 
   //1000 points on the contour
-  static int POINTS = 1000;
+  int POINTS = 1000;
 
-  float * julia_set_x = malloc(POINTS * sizeof(float));
-  float * julia_set_y = malloc(POINTS * sizeof(float));
+  long double * julia_set_x = malloc(POINTS * sizeof(long double));
+  long double * julia_set_y = malloc(POINTS * sizeof(long double));
 
-  //plot julia contour between -1.5, 1.5
+  long double init_x = -1.5;
+  long double init_y = -1.5;
+  long double step = 0.003;
   int count = 0;
-  long double temp_x = -1.5;
   for(int x = 0; x < POINTS; x++){
-    temp_x = temp_x + 0.001;
-    long double temp_y = -1.5;
+    init_x = (long double)(init_x + step);
+    init_y = -1.5;
     for(int y = 0; y < POINTS; y++){
-      temp_y = temp_y + 0.001;
-      if(julia_element(temp_x, temp_y) < 10){
-        julia_set_x[count] = (float)temp_x;
-        julia_set_y[count] = (float)temp_y;
-        count = count + 1;
-        printf("%f\n", julia_set_x[count]);
+      init_y = (long double)(init_y + step);
+      int current_julia = julia_element(init_x, init_y);
+      if(current_julia < 10 && count < POINTS){
+        julia_set_x[count] = init_x;
+        julia_set_y[count] = init_y;
       }
     }
+    count++;
   }
 
-  for(int i = 0; i < POINTS; i++){
-    printf("x: %f, y: %f\n", julia_set_x[i], julia_set_y[i]);
-  }
+  scrmod("revers");
+  setpag("da4l");
+  metafl("cons");
+  disini();
+  pagera();
 
-  scrmod ("revers");
-  setpag ("da4l");
-  metafl ("cons");
-  disini ();
-  pagera ();
+  titlin("Julia Set", 1);
+  titlin("F(Z) = Z*Z - 0.8 + 0.156i", 3);
 
-  titlin ("Julia Set", 1);
-  titlin ("F(Z) = Z*Z - 0.8 + 0.156i", 3);
-
-  name   ("R-axis", "x");
-  name   ("I-axis", "y");
-
+  name("R-axis", "x");
+  name("I-axis", "y");
 
   qplsca(julia_set_x, julia_set_y, POINTS);
+
+  free(julia_set_x);
+  free(julia_set_y);
 
   return 0;
 }
