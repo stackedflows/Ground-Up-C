fac3d74cbd0ae60cde71af72be35eda3b626f448
Kai <73109076+stackedflows@users.noreply.github.com>
2021-02-21 02:23:13 +0000

    Update julia.c

--- DISLIN/julia.c
+++ DISLIN/julia.c
@@ -3,20 +3,51 @@
 #include <math.h>
 #include "dislin.h"
 
-#define STEPS 10000
+#define J_STEPS 10
+
+//generate jula set data for particular constant
+static long double const_x;
+static long double const_y;
+long double * r_lim;
 
 long double julia_x(long double x, long double y){
-  return x*x - y*y;
+  return x * x - y * y;
 }
 long double julia_y(long double x, long double y){
-  return 2*x*y + ;
+  return 2 * x * y;
 }
 
 long double _abs(long double x, long double y){
-  return sqrt((x * x) + (y * y));
+  return sqrt(x * x + y * y);
 }
 
-void plot_j(float * x, float * y){
+int julia_element(long double x, long double y){
+
+  long double * julia_element_x = malloc(J_STEPS * sizeof(long double));
+  long double * julia_element_y = malloc(J_STEPS * sizeof(long double));
+  julia_element_x[0] = x;
+  julia_element_y[0] = y;
+
+  int count = 0;
+  for(int i = 0; i < J_STEPS; i++){
+    if(_abs(julia_element_x[i], julia_element_y[i]) > r_lim[0]){
+      break;
+    }
+    else if(_abs(julia_element_x[i], julia_element_y[i]) < r_lim[1]){
+      break;
+    }
+    else{
+      long double next_x = julia_x(julia_element_x[i], julia_element_y[i]);
+      long double next_y = julia_y(julia_element_x[i], julia_element_y[i]);
+      julia_element_x[i + 1] = next_x + const_x;
+      julia_element_y[i + 1] = next_y + const_y;
+    }
+    count = i;
+  }
+  return count;
+}
+
+void plot_j(float * x, float * y, int points){
   scrmod ("revers");
   setpag ("da4l");
   metafl ("cons");
@@ -29,44 +60,39 @@ void plot_j(float * x, float * y){
   name   ("R-axis", "x");
   name   ("I-axis", "y");
 
-  graf(-100.0, 100.0, -100.0, 10, -100.0, 10, -100.0, 100.0);
-
-  qplsca(x, y, STEPS);
+  graf(-1.5, 1.5, -1.5, 0.5, -1.5, 1.5, -1.5, 0.5);
+  qplsca(x, y, J_STEPS * 2);
 }
 
 int main(void){
-
-  //generate jula set data for particular constant
-  static long double const_x = -0.8;
-  static long double const_y = 0.156;
-
+  //constants
+  const_x = -0.8;
+  const_y = 0.156;
   long double radius = _abs(const_x, const_y);
+  r_lim = malloc(2 * sizeof(long double));
+  r_lim[0] = 1.5320213637808087; //solves r_lim(r_lim - 1) = radius
+  r_lim[1] = -0.5320213637808087;
 
-  static long double r_lim = 1.5320213176092827; //solves r_lim(r_lim - 1) = radius
+  //plot contour of julia set
+  long double * julia_set_x = malloc(100 * sizeof(long double));
+  long double * julia_set_y = malloc(100 * sizeof(long double));
+  julia_set_x[0] = 0.0;
+  julia_set_y[0] = 0.0;
 
-  long double * julia_element_x = malloc(STEPS * sizeof(long double));
-  long double * julia_element_y = malloc(STEPS * sizeof(long double));
-  julia_element_x[0] = 0.0;
-  julia_element_y[0] = 0.0;
+  for(int x = 0; x < 1000; x++){
+    for(int y = 0; y < 1000; y++){
 
-  for(int i = 0; i < STEPS; i++){
-    if(_abs(julia_element_x[i], julia_element_y[i]) > r_lim){
-      break;
-    }
-    else{
-      long double next_x = julia_x(julia_element_x[i], julia_element_y[i]);
-      long double next_y = julia_y(julia_element_x[i], julia_element_y[i]);
-      julia_element_x[i + 1] = next_x + const_x;
-      julia_element_y[i + 1] = next_y + const_y;
+      if(julia_element(julia_set_x[x], julia_set_y[y] < 10)){
 
+        count = count + 1;
+      }
     }
   }
 
+  plot_j(x, y, 100);
+
   //plot results
   //plot_j((float *)julia_element_x, (float *)julia_element_x);
-
-  free(julia_element_x);
-  free(julia_element_y);
 
   return 0;
 }
