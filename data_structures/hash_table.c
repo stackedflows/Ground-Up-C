d79cadee3d1ee9617d238b90edfed1cb5a1c541d
Kai <73109076+stackedflows@users.noreply.github.com>
2021-03-16 20:08:35 +0000

    Create hash_table.c

---
+++ data_structures/hash_table.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 10
+
+struct hash_obj{
+	int data;
+	int key;
+};
+
+struct * hash_table;
+
+int hash_func(int key){
+	
+	return key % SIZE;
+}
+
+struct hash_obj * search(int key){
+
+}
+
+void insert(int * key_pos, int data){
+
+}
+
+struct hash_obj * delete(struct hash_obj * item){
+
+}
+
+
+int main(void){
+	
+	hash_table = malloc(SIZE * sizeof(struct));
+	
+	return 0;
+}
