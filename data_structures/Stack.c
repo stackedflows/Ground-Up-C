05b0fe40689ee3ac46d5e3bde57bc411f369bb92
Kai <73109076+stackedflows@users.noreply.github.com>
2021-03-16 19:42:07 +0000

    Create Stack.c

---
+++ data_structures/Stack.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 10
+
+int * stack;
+int count;
+
+void push(int current, int count){
+	if(count == SIZE){
+		return;
+	}
+	else{
+		count++;
+		for(int i = 0; i < count; i++){
+			stack[count - i] = stack[count - i - 1];
+		}
+		stack[0] = current;
+	}
+}
+
+int pop(int count){
+	if(count == 0){
+		return;
+	}
+	else{
+		int pop = stack[0];
+		for(i = 0; i < count; i++){
+			stack[i] = stack[i + 1];
+		}
+	}
+}
+
+int main(void){
+	
+	count = 0;
+	stack = malloc(SIZE * sizeof(int));
+
+	return 0;
+}
