cad1e6f0b5e0b0e5902e975ea952716b17642560
Kai <73109076+stackedflows@users.noreply.github.com>
2021-03-17 14:48:04 +0000

    Update hash_table.c

--- data_structures/hash_table.c
+++ data_structures/hash_table.c
@@ -16,53 +16,20 @@ int hash_func(int key){
 }
 
 struct hash_obj * search(int key){
-	for(int i = 0; i < SIZE; i++){
-		i %= SIZE;
-		if(hash_table[i] -> key == key){
-			return 	hash_table[i];
-		}else{
-			printf("probe");
-			return NULL;
-		}
-	}
+	
 }
 
 void insert(int key, int data){
-	struct hash_obj * insert = (struct hash_obj *)malloc(sizeof(struct hash_obj));
-	hash_obj -> key = key;
-	hash_obj -> data = data;
+	struct hash_obj * new_obj = (struct has_obj *)malloc(sizeof(struct has_obj));
+	new_obj -> key = key;
+	new_obj -> data = data;
 	
 	int code = hash_func(key);
-	
-	for(int i = 0; i < SIZE; i++){
-		i %= SIZE;
-		if(hash_table[i] -> key == code && hash_table[i] == NULL){
-			hash_table[i] = hash_obj;	
-		}else{
-			printf("probe");
-			return;
-		}
-	}
 }
 
 struct hash_obj * delete(struct hash_obj * item){
-	int key = hash_obj -> key;
-	int index = hash_func(key);
+	int key = item -> key;
 	
-	struct hash_obj * hash_dummy = (struct hash_obj *)malloc(sizeof(struct hash_obj));
-	
-	for(int i = 0; i < SIZE; i++){
-		i %= SIZE;
-		if(hash_table[i] -> key = index){
-			struct hash_obj * hash_temp = hash_table[index];
-			
-			hash_table[index] = hash_dummy;
-			return hash_temp;
-		}else{
-			printf("probe");
-			return NULL;
-		}
-	}
 }
 
 int main(void){
