3941b82d77c74e21d156bd18fe3c2c43da97bfe8
Kai <73109076+stackedflows@users.noreply.github.com>
2021-03-17 12:46:46 +0000

    Update radix.c

--- algos/radix.c
+++ algos/radix.c
@@ -8,34 +8,26 @@ int ** Queues;
 
 float max_digit;
 
-int max(int * array){
-	min=max=array[0];
-	for(i=1; i<n; i++){
-		if(min>array[i]){
-			min=array[i];  
-		}
-		if(max<array[i]){
-			max=array[i];
-		}
+int num_digits(int digit){
+	int digit = digit;
+	dig_count = -1;
+	while(digit > 1){
+		digit = digit / 10;
+		dig_count++;
 	}
-	return max;
 }
 
 int main(void){
 	int * unsorted = malloc(SIZE_list * sizeof(int));
 	Queues = malloc(SIZE_queue * sizeof(SIZE_list));
 
-	max_digit = (float)max(unsorted);
-	dig_count = -1;
-	while(max_digit > 1){
-		max_digit = max_digit / 10;
-		dig_count++;
-	}
 	for(int i = 0; i < unsorted; i++){
 		for(ii = 1; ii <= 10; i++){
-			if(unsorted[i] % ii == 0){
+			int dig_count = num_digits(unsorted[i]);
+			if(unsorted[i] % ii == 0 && dig_count > ii){
 				insert(Queues[ii], unsorted[i]);
 			} 
 		}
 	}
+	//to be extended
 }
