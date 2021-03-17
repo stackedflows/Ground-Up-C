5f001ec3d238c7acfe1d65184a0e26819d25a078
Kai <73109076+stackedflows@users.noreply.github.com>
2021-03-17 15:42:12 +0000

    Update hash_table.c

--- data_structures/hash_table.c
+++ data_structures/hash_table.c
@@ -15,24 +15,65 @@ int hash_func(int key){
 }
 
 struct hash_obj * search(int key){
-	
+	int code = hash_func(key);
+	int probe = code;
+	while(hash_table[probe] == NULL){
+		if(hash_table[probe] -> key == key && hash_table[probe] !== NULL){
+			return hash_table[probe];
+		}
+		if(probe > SIZE){
+			printf("overflow\n");
+			return NULL;
+		}else{
+			++probe;
+			probe %= SIZE;
+		}
+	}
 }
 
 void insert(int key, int data){
 	struct hash_obj * new_obj = (struct has_obj *)malloc(sizeof(struct has_obj));
 	new_obj -> key = key;
 	new_obj -> data = data;
-	
+
 	int code = hash_func(key);
+	int probe = code;
+	while(hash_table[probe] != NULL){
+		if(probe > SIZE){
+			printf("overflow\n");
+			return NULL;
+		}else{
+			++probe;
+			probe %= SIZE;
+		}
+	}
+	hash_table[probe] = new_obj;
 }
 
 struct hash_obj * delete(struct hash_obj * item){
 	int key = item -> key;
-	
+	int code = hash_func(key);
+
+	int probe = code;
+	while (hash_table[probe] == NULL) {
+		if(probe > SIZE){
+			printf("overflow\n");
+			return NULL;
+		}else{
+			++probe;
+			probe %= SIZE;
+		}
+	}
+
+	struct hash_obj * dummy;
+	dummy -> key = -1;
+	dummy -> data = -1;
+
+	hash_table[probe] = dummy;
 }
 
 int main(void){
-	
+
 	hash_table = malloc(SIZE * sizeof(struct hash_obj));
 	//do stuff
 	free(hash_table);
