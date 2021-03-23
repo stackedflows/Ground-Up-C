b383db0654d04d27198f36f2ad7fc70e756bca1c
Kai <73109076+stackedflows@users.noreply.github.com>
2021-03-23 01:24:23 +0000

    Create functions.c

---
+++ DISLIN/E8/functions.c
@@ -0,0 +1,5 @@
+
+// 1) We will need a function that takes all vertices and permutes the vector, then returns all 2 - D projections
+
+// 2) We will need a function that runs through all the root verticies and calculates minimum distances one at a time, then groups minimum
+//distances for each vertex and produces a matrix based on these
