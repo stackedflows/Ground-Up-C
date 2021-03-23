d046440762410da70866fa3bbfe6465cac990777
Kai <73109076+stackedflows@users.noreply.github.com>
2021-03-23 00:17:43 +0000

    Create functions.c

---
+++ DISLIN/functions.c
@@ -0,0 +1,4 @@
+// 1) We will need a function that takes all vertices and permutes the vector
+
+// 2) We will need a function that runs through all the root verticies and calculates minimum distances one at a time, then groups minimum
+//distances for each vertex and produces a matrix based on these
