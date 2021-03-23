c3be88dacc959ab6d5535601c3d37f658ff679c8
Kai <73109076+stackedflows@users.noreply.github.com>
2021-03-23 21:04:52 +0000

    Update gen.c

--- DISLIN/E8/gen.c
+++ DISLIN/E8/gen.c
@@ -1,2 +1,27 @@
+//gcc gen.c -o g
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define DIM 8
 
-// 1) uses the functions script to generate specialised data for e8 base vectors
+void permute_0(float * array){
+
+}
+
+void permute_1(float * array){
+  
+}
+
+int main(void){
+
+  float e1[DIM] = {1,0,0,0,0,0,0,0};
+  float e2[DIM] = {0,1,0,0,0,0,0,0};
+  float e3[DIM] = {0,0,1,0,0,0,0,0};
+  float e4[DIM] = {0,0,0,1,0,0,0,0};
+  float e5[DIM] = {0,0,0,0,1,0,0,0};
+  float e6[DIM] = {0,0,0,0,0,1,0,0};
+  float e7[DIM] = {0,0,0,0,0,0,1,0};
+  float e8[DIM] = {0,0,0,0,0,0,0,1};
+
+  return 0;
+}
