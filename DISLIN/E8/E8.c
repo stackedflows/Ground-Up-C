14f15351a9ed75dd4e18568a4b88a51380782a43
Kai <73109076+stackedflows@users.noreply.github.com>
2021-03-25 12:54:24 +0000

    Fixed and working!

--- DISLIN/E8/E8.c
+++ DISLIN/E8/E8.c
@@ -15,12 +15,13 @@ int shouldSwap(float base[], int start, int curr)
     return 1;
 }
 
-void permutations(float base[], int index, int n, float target[][8])
+float root_sys[240][8];
+
+void permutations(float base[], int index, int n)
 {
     if (index >= n) {
         for(int i = 0; i < n; i++){
-          target[count][i] = base[i];
-          //printf("%f ", base[i]);
+          root_sys[count][i] = base[i];
         }
         count++;
         printf("\n");
@@ -34,7 +35,7 @@ void permutations(float base[], int index, int n, float target[][8])
             float temp_1 = base[i];
             base[index] = temp_1;
             base[i] = temp_0;
-            permutations(base, index + 1, n, target);
+            permutations(base, index + 1, n);
             float temp_2 = base[index];
             float temp_3 = base[i];
             base[index] = temp_3;
@@ -74,108 +75,41 @@ void print_p(float arr[][8], int size){
 
 int main(void){
 
-  float root_system[240][8];
-
   // base vector permutations of E8 root system
 
   count = 0;
   float b1[8] = {1,1,0,0,0,0,0,0};
-  float type_b1[28][8];
-  permutations(b1, 0, 8, type_b1);
-  for(int i = 0; i < 28; i++){
-    for(int ii = 0; ii < 8; ii++){
-        printf("%f ", type_b1[i][ii]);
-      }
-      printf("\n");
-  }
+  permutations(b1, 0, 8);
   printf("\n");
 
-  count = 0;
   float b2[8] = {1,-1,0,0,0,0,0,0};
-  float type_b2[56][8];
-  permutations(b2, 0, 8, type_b2);
-  for(int i = 28; i < 84; i++){
-    for(int ii = 0; ii < 8; ii++){
-        printf("%f ", type_b2[i][ii]);
-      }
-      printf("\n");
-  }
+  permutations(b2, 0, 8);
   printf("\n");
 
-  count = 0;
   float b3[8] = {-1,-1,0,0,0,0,0,0};
-  float type_b3[28][8];
-  permutations(b3, 0, 8, type_b3);
-    for(int i = 84; i < 112; i++){
-      for(int ii = 0; ii < 8; ii++){
-        printf("%f ", type_b3[i][ii]);
-      }
-      printf("\n");
-  }
+  permutations(b3, 0, 8);
   printf("\n");
 
-  count = 0;
   float b4[8] = {0.5,0.5,-0.5,-0.5,-0.5,-0.5,-0.5,-0.5};
-  float type_b4[28][8];
-  permutations(b4, 0, 8, type_b4);
-  for(int i = 112; i < 140; i++){
-    for(int ii = 0; ii < 8; ii++){
-        printf("%f ", type_b4[i][ii]);
-      }
-      printf("\n");
-  }
+  permutations(b4, 0, 8);
   printf("\n");
 
-  count = 0;
   float b5[8] = {0.5,0.5,0.5,0.5,-0.5,-0.5,-0.5,-0.5};
-  float type_b5[70][8];
-  permutations(b5, 0, 8, type_b5);
-  for(int i = 140; i < 210; i++){
-    for(int ii = 0; ii < 8; ii++){
-      printf("%f ", type_b5[i][ii]);
-    }
-    printf("\n");
-  }
+  permutations(b5, 0, 8);
   printf("\n");
 
-  count = 0;
   float b6[8] = {0.5,0.5,0.5,0.5,0.5,0.5,-0.5,-0.5};
-  float type_b6[28][8];
-  permutations(b6, 0, 8, type_b6);
-  for(int i = 210; i < 238; i++){
-    for(int ii = 0; ii < 8; ii++){
-      printf("%f ", type_b6[i][ii]);
-    }
-    printf("\n");
-  }
+  permutations(b6, 0, 8);
   printf("\n");
 
-  float type_b7[8] = {0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5};
-  float type_b8[8] = {-0.5,-0.5,-0.5,-0.5,-0.5,-0.5,-0.5,-0.5};
-
+  float b7[8] = {0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5};
+  float b8[8] = {-0.5,-0.5,-0.5,-0.5,-0.5,-0.5,-0.5,-0.5};
 
   for(int i = 0; i < 8; i++){
-    root_system[238][i] = type_b7[i];
-    root_system[239][i] = type_b8[i];
+    root_sys[238][i] = b7[i];
+    root_sys[239][i] = b8[i];
   }
 
-  printf("test\n");
-
-  for(int i = 0; i < 240; i++){
-    if(i == 28)printf("\n");
-    if(i == 84)printf("\n");
-    if(i == 112)printf("\n");
-    if(i == 140)printf("\n");
-    if(i == 210)printf("\n");
-    if(i == 238)printf("\n");
-    if(i == 239)printf("\n");
-    for(int ii = 0; ii < 8; ii++){
-      //printf("%f ", root_system[i][ii]);
-    }
-    printf("\n");
-  }
-
-  printf("end\n");
   //
 
   printf("\n");
@@ -187,21 +121,17 @@ int main(void){
   int distance_matrix[240][240];
 
   for(int i = 0; i < 240; i++){
+    int c_count = 0;
+    int dist_m = 100;
     for(int ii = 0; ii < 240; ii++){
-      float dist = (int)inner_product(root_system[i], root_system[ii]);
+      float dist = (int)inner_product(root_sys[i], root_sys[ii]);
       if(dist == 2){
         distance_matrix[i][ii] = 1;
+        c_count++;
       }else{distance_matrix[i][ii] == 0;};
     }
+    printf("%d\n", c_count);
   }
-
-  printf("\n");
-
-  printf("\n");
-
-  printf("\n");
-
-  printf("\n");
 
   return 0;
 }
