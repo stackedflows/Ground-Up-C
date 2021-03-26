04c304b68671742fef8e40307bbc6fed5c53aad0
Kai <73109076+stackedflows@users.noreply.github.com>
2021-03-26 20:53:02 +0000

    Update eig.py

--- DISLIN/E8/eig.py
+++ DISLIN/E8/eig.py
@@ -1,3 +1,16 @@
+# using online tool to calculate base from basis
+#e0 = (1-1000000)
+#e1 = (01-100000)
+#e2 = (001-10000)
+#e3 = (0001-1000)
+#e4 = (00001-100)
+#e5 = (00000110)
+#e6 = (-.5-.5-.5-.5-.5-.5-.5-.5)
+#e7 = (000001-10)
+# eigval RE = cos(0.209440); //cos(PI/15)
+# eigval IM = sin(0.209440); //sin(PI/15)
+#
+
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.linalg as la
