aff86525cdd2ada365fe29b416bda044a0c438aa
Kai <73109076+stackedflows@users.noreply.github.com>
2021-03-26 20:42:47 +0000

    Update E8.c

--- DISLIN/E8/E8.c
+++ DISLIN/E8/E8.c
@@ -44,18 +44,6 @@ void permutations(float base[], int index, int n)
 
 // matrix operations
 
-float r_c[8][8];
-void mat_multiply(float mat1[][8], float mat2[][8]){
-  for (int i = 0; i < 8; i++){
-      for (int ii = 0; ii < 8; ii++){
-          r_c[i][ii] = 0;
-          for (int iii = 0; iii < 8; iii++){
-            r_c[i][ii] += mat1[i][iii] * mat2[iii][ii];
-          }
-      }
-  }
-}
-
 // function to list all distances from one node to others
 
 float inner_product(float * vect_0, float * vect_1){
@@ -102,46 +90,6 @@ int main(void){
     }
   }
 
-  // calculating r_c
-
-  float weyl_group[8][8][8];
-
-  for(int i = 0; i < 8; i++){
-    for(int ii = 0; ii < 8; ii++){
-      for(int iii = 0; iii < 8; iii++){
-        if(ii == iii){
-          weyl_group[i][ii][iii] = 1 - sqrt(2) * base_sys[i][ii] * base_sys[i][iii];
-        }
-        else{
-          weyl_group[i][ii][iii] = -sqrt(2) * base_sys[i][ii] * base_sys[i][iii];
-        }
-      }
-    }
-  }
-
-  float init[8][8];
-  for(int i = 0; i < 8; i++){
-    for(int ii = 0; ii < 8; ii++){
-      init[i][ii] = weyl_group[0][i][ii];
-    }
-  }
-
-  float load[8][8];
-  for(int i = 1; i < 8; i++){
-    for(int ii = 0; ii < 8; ii++){
-      for(int iii = 0; iii < 8; iii++){
-        load[i][ii] = weyl_group[i][i][ii];
-      }
-    }
-    mat_multiply(init, load);
-  }
-
-  for(int i = 0; i < 8; i++){
-    for(int ii = 0; ii < 8; ii++){
-      printf("%f ", r_c[i][ii]);
-    }
-    printf("\n");
-  }
 
   //gcc combo.c -o c
 
