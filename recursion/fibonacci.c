58fba2b9a5b85e373682ae865a033bf0300c7ee6
Kai <73109076+stackedflows@users.noreply.github.com>
2021-04-13 19:50:57 +0100

    Update fibonacci.c

--- recursion/fibonacci.c
+++ recursion/fibonacci.c
@@ -3,8 +3,8 @@
 void fib(int a, int b){
     int x = b;
     int y = b + a;
-    printf("%d\n", y);
-    if( y < 10){
+    printf("%d ", y);
+    if( y < 100){
         fib(x, y);
     }else{
         return;
