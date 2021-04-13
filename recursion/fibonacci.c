478ec99c0748d88f26b5e6c049d3c0a693d056d0
Kai <73109076+stackedflows@users.noreply.github.com>
2021-04-13 20:01:07 +0100

    Update fibonacci.c

--- recursion/fibonacci.c
+++ recursion/fibonacci.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
-void fib(int a, int b, int n){
+void fib(int a, int b, int n, int max){
+    int m = max;
     int count = n + 1;
     int x = b;
     int y = b + a;
-    if( count < 40){
-        fib(x, y, count);
+    if( count < m){
+        fib(x, y, count, m);
         printf("%d ", x);
     }
 }
 
 int main (void){
-  fib (0, 1, 0);
+  fib (0, 1, 0, 40);
   return 0;
 }
