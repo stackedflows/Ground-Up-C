a7a847da88f9e7adee5e0e302a958f4aca7b207f
Kai <73109076+stackedflows@users.noreply.github.com>
2021-05-02 13:20:20 +0100

    Update v_fundamental.c

--- algos/sorting/v_fundamental.c
+++ algos/sorting/v_fundamental.c
@@ -67,20 +67,20 @@ void heapify(int arr[], int n, int i) {
 
     // find largest of 3 elements
     // Find largest among root, left child and right child
-    int largest = i;
+    int root = i;
     int left = 2 * i + 1;
     int right = 2 * i + 2;
   
-    if (left < n && arr[left] > arr[largest])
-      largest = left;
+    if (left < n && arr[left] > arr[root])
+      root = left;
   
-    if (right < n && arr[right] > arr[largest])
-      largest = right;
+    if (right < n && arr[right] > arr[root])
+      root = right;
   
     // Swap and continue heapifying if root is not largest
-    if (largest != i) {
-      swap(&arr[i], &arr[largest]);
-      heapify(arr, n, largest);
+    if (root != i) {
+      swap(&arr[i], &arr[root]);
+      heapify(arr, n, root);
     }
 }
 
