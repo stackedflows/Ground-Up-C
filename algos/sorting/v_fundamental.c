5d67bf78eab843bd814f50e0b02fc86529cdd120
Kai <73109076+stackedflows@users.noreply.github.com>
2021-05-02 13:18:27 +0100

    Update v_fundamental.c

--- algos/sorting/v_fundamental.c
+++ algos/sorting/v_fundamental.c
@@ -49,3 +49,53 @@ void bubbleSort(int arr[], int n)
         }
     }    
 }
+
+//Heap sort
+
+ void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+  }
+
+void heapify(int arr[], int n, int i) {
+    // Find largest among root, left child and right child
+    
+    // root = arr[i]
+    // left child of arr[i] = arr[2i + 2]
+    // right child of arr[i] = arr[2i + 1]
+
+    // find largest of 3 elements
+    // Find largest among root, left child and right child
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+  
+    if (left < n && arr[left] > arr[largest])
+      largest = left;
+  
+    if (right < n && arr[right] > arr[largest])
+      largest = right;
+  
+    // Swap and continue heapifying if root is not largest
+    if (largest != i) {
+      swap(&arr[i], &arr[largest]);
+      heapify(arr, n, largest);
+    }
+}
+
+// Main function to do heap sort
+void buildHeap(int arr[], int n) {
+    
+    // Build max heap
+    for (int i = n / 2 - 1; i >= 0; i--)
+      heapify(arr, n, i);
+  
+    // Heap sort
+    for (int i = n - 1; i >= 0; i--) {
+      swap(&arr[0], &arr[i]);
+  
+      // Heapify root element to get highest element at root again
+      heapify(arr, i, 0);
+    }
+}
