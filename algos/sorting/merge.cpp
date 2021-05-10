4da7376d0cbf636cff3d0a5257f22a54a87de1c8
Kai <73109076+stackedflows@users.noreply.github.com>
2021-05-10 14:56:27 +0100

    Update merge.cpp

--- algos/sorting/merge.cpp
+++ algos/sorting/merge.cpp
@@ -1,15 +1,36 @@
-//Function to sort the array using bubble sort algorithm.
-void bubbleSort(int arr[], int n)
+class Solution
 {
-    for(int i = 0; i < n - 1; i++){
-        int curr = arr[i];
-        int temp = arr[i + 1];
-        int count = 0;
-        while(curr > temp){
-            arr[i + 1] = curr;
-            arr[i] = temp;
-            temp = arr[i + count + 1];
-            count++;
+    public:
+    void merge(int arr[], int l, int m, int r)
+    {
+        
+        int n1 = m - l + 1;
+        int n2 = r - m;
+        
+        int L[n1], M[n2];
+        
+        // copy arrays from 
+        for(int i = 0; i < n1; i++){
+            L[i] = arr[l + i];
         }
-    }    
-}
+        for(int i = 0; i < n2; i++){
+            M[i] = arr[m + 1 + i];
+        }
+        
+        
+    }
+    public:
+    void mergeSort(int arr[], int l, int r)
+    {
+        // 
+        if(l >= r){
+            return;
+        }
+        // calculate new midpoint and seperate arrays
+        int mid = l + (r - 1) / 2;
+        mergeSort(arr, l, mid);
+        mergeSort(arr, mid + 1, r);
+        // merge new sorted arrays
+        merge(arr, l, mid, r);
+    }
+};
