eccf8296ff00a29644b31f9cfc08fa9648f898a4
Kai <73109076+stackedflows@users.noreply.github.com>
2021-05-10 15:48:51 +0100

    Update merge.cpp

--- algos/sorting/merge.cpp
+++ algos/sorting/merge.cpp
@@ -3,34 +3,65 @@ class Solution
     public:
     void merge(int arr[], int l, int m, int r)
     {
-        
+        //new array sizes
         int n1 = m - l + 1;
         int n2 = r - m;
         
-        int L[n1], M[n2];
+        // arrays to be sorted
+        int L[n1], R[n2];
         
-        // copy arrays from 
+        // copy arrays within recursive restraints
         for(int i = 0; i < n1; i++){
             L[i] = arr[l + i];
         }
-        for(int i = 0; i < n2; i++){
-            M[i] = arr[m + 1 + i];
+        for(int j = 0; j < n2; j++){
+            R[j] = arr[m + 1 + j];
         }
         
+        // merge arrays back into main array
+        //tracks array L
+        int i = 0;
+        // tracks array M
+        int j = 0;
+        // tracks overall array
+        int k = l;
         
+        // will sort the main part of the output array
+        while(i < n1 && j < n2){
+            if(L[i] <= R[j]){
+                arr[k] = L[i];
+                i++;
+            }else{
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+        
+        // handle remaining L elements
+        while(i < n1){
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+        
+        // handle remaining M elements
+        while(j < n2){
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
     }
+    
     public:
     void mergeSort(int arr[], int l, int r)
     {
-        // 
-        if(l >= r){
-            return;
+        if(l>=r){
+            return;//returns recursively
         }
-        // calculate new midpoint and seperate arrays
-        int mid = l + (r - 1) / 2;
-        mergeSort(arr, l, mid);
-        mergeSort(arr, mid + 1, r);
-        // merge new sorted arrays
-        merge(arr, l, mid, r);
+        int m =l+ (r-l)/2;
+        mergeSort(arr,l,m);
+        mergeSort(arr,m+1,r);
+        merge(arr,l,m,r);
     }
 };
