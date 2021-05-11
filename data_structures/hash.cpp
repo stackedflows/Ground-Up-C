49b3cf2ccd4790178f3cfc9064be9d8b87c75648
Kai <73109076+stackedflows@users.noreply.github.com>
2021-05-11 17:04:32 +0100

    Create hash.cpp

---
+++ data_structures/hash.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+// max size of hash map
+#define MAX 1000
+
+// hash map
+int hash_map[MAX][2];
+
+// search for element in array
+bool search(int X){
+  
+  // X positive
+  if(X >= 0){
+    if(hash_map[X][0] == 1){
+        return true; 
+     }else{
+        return false;
+      }
+  }  
+  
+  // X negative
+  if(hash_map[abs(X)][1] == 1){
+    return true; 
+  }else{
+    return false;
+  }
+}
+
+// insert method
+void insert(int a[], int n){
+   for(int i = 0; i < n; i++){
+     if(a[i] > 0){
+       hash_map[a[i]][0] = 1;
+     }else{
+       hash_map[abs(a[i])][1] = 1;
+     }
+   }
+}
+
+int main(){
+    int a[] = { -1, 9, -5, -8, -5, -2 };
+    int n = sizeof(a)/sizeof(a[0]);
+    insert(a, n);
+    int X = -5;
+    if (search(X) == true)
+       cout << "Present"; 
+    else
+       cout << "Not Present";
+    return 0
