3aa76fd893ef18e33876abfe76949c30c7c91945
Kai <73109076+stackedflows@users.noreply.github.com>
2021-05-11 12:12:17 +0100

    Create heap.cpp

---
+++ data_structures/heap.cpp
@@ -0,0 +1,56 @@
+// impliments a bubble sort with an array ordered as a binary tree
+
+#include <iostream>
+
+using namespace std;
+
+#define MAX 100
+
+// in this array, each node has left child 2i + 1, right child 2i + 2
+// each node has parent floor((i-1)/2)
+int arr[MAX];
+
+// tracks how many elements are inserted into heap
+int count = 0;
+
+// sorts heap
+void sort(int position){
+    if(count == 0){return;}
+    else{
+        int i = position - 1;
+        int j = (position-1)/2;
+        while(arr[j] > arr[i]){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i = (i-1)/2;
+        }
+    }
+}
+
+// handles insert
+void insert(int data){
+    if(count == MAX){
+        return;
+    }else{
+        arr[count] = data;
+        count++;
+    }
+    sort(count);
+}
+
+int main(){
+    
+    insert(10);
+    insert(5);
+    insert(13);
+    insert(17);
+    
+
+    for(int i = 0; i < count; i++){
+        cout << arr[i];
+        cout << "\n";
+    }
+
+    return 0;
+}
