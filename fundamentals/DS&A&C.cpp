70daaf78b121fb19ce40b835f4195f7e06124a5e
Kai <73109076+stackedflows@users.noreply.github.com>
2021-05-22 01:14:16 +0100

    Update DS&A&C.cpp

--- fundamentals/DS&A&C.cpp
+++ fundamentals/DS&A&C.cpp
@@ -1,282 +1,295 @@
 #include <iostream>
-
 using namespace std;
 
-//// class for sorting algorithms and their complexity analysis
 class sort {
-public:
-    
-    // Heap sort implimentatiion and complexity analysis
-    void heapify(int a[], int len, int x) {
-        int largest = x; // O(1)
-        int left = 2 * x + 1; // O(1)
-        int right = 2 * x + 2;  // O(1)
+	public :
+		void heapify(int a[], int len, int x){
+			int largest = x;
+			int left = 2 * x + 1;
+			int right = 2 * x + 2;
 
-        if (left < len && a[left] > a[largest]) {
-            largest = left; 
-        }
-        if (right < len && a[right] > a[largest]) {
-            largest = right; 
-        }
-        if (largest != x) {
-            swap(a[x], a[largest]); // O(1)
-            heapify(a, len, largest); // O(log(n))
-        }
-        // worst case time: left and right are at the bottom of the tree, and are the largest elements, then: O(log(n))
-        // worst case space: O(1)
-    }
-    
-    void heapsort(int a[], int len) {
-        for (int i = (len / 2) - 1; i >= 0; i--) {
-            heapify(a, len, i);
-        }  // worst case time: (n/2) * log(n) 
-        // worst case space: O(1)
-        for (int i = len - 1; i >= 0; i--) {
-            swap(a[i], a[0]);
-            heapify(a, i, 0);
-        } // worst case time: n * log(n)
-        // worst case space: O(n)
+			if (left < len && a[left]> a[largest]) {
+				largest = left;
+			}
+			if (right < len && a[right] > a[largest]) {
+				largest = right;
+			}
+			if (largest != x) {
+				swap(a[largest], a[x]);
+				heapify(a, len, largest);
+			}
+		}
 
-        // total asymptotic time complexity: O(nlogn)
-        // total asymptotic space complexity: O(n)
-    }
+		void heapsort(int a[], int len) {
+			for (int i = (len / 2) - 1; i >= 0; i--) {
+				heapify(a, len, i);
+			}
+			for (int i = len - 1; i >= 0; i--) {
+				swap(a[i], a[0]);
+				heapify(a, i, 0);
+			}
+		}
 
-    // Merge sort implimentation and analysis
-    void merge(int l[], int nl, int r[], int nr, int a[]) {
-        int i = 0; // O(1)
-        int j = 0; // O(1)
-        int k = 0; // O(1)
+		// space complexity: O(1), as the algorithm only alters the original array, throught heapifying and comparing
+		// time complexity: worst case O(nlog(n)), as the heap has height log(n), and heapify is called n times
 
-        while (i < nl && j < nr) {
-            if (l[i] < r[j]) {
-                a[k] = l[i];
-                i++;
-            }
-            else {
-                a[k] = r[j];
-                j++;
-            }
-            k++;
-        } // worst case time complexity: l[0]/r[0] is the smallest element: max(nl, nr) iterations
-        while (i < nl) {
-            a[k] = l[i];
-            i++;
-            k++;
-        } // worst case time complexity: nr == 0, then we set O(nl)
-        while (j < nr) {
-            a[k] = r[j];
-            j++;
-            k++;
-        } // worst case time complexity: nl == 0, then we set O(nr)
+		int partition(int a[], int start, int end) {
+			int pivot = a[end];
+			int index = start;
 
-        // total time complexity: O(n)
-        // total space complexity O(1)
-    }
+			for (int i = start; i < end; i++) {
+				if (a[i] > pivot) {
+					swap(a[i], a[index]);
+					index++;
+				}
+			}
 
-    void mergesort(int a[], int len) {
-        if (len < 2)return;
-        int mid = len / 2; // O(1)
-        int* l = new int[mid](); // O(n)
-        int* r = new int[len - mid](); // O(n)
+			swap(a[end], a[index]);
+			return index;
+		}
 
-        for (int i = 0; i < mid; i++) {
-            l[i] = a[i];
-        } // worst case time complexity: O(n)
-        for (int i = mid; i < len; i++) {
-            r[i - mid] = a[i];
-        } // worst case time complexity: O(n)
+		void quicksort(int a[], int start, int end) {
+			if (start < end) {
+				int pivot = partition(a, start, end);
 
-        mergesort(l, mid); // called untill mid < 2: O(log(n))
-        mergesort(r, len - mid); // O(log(n))
-        merge(l, mid, r, len - mid, a); // O(n)
+				quicksort(a, start, pivot - 1);
+				quicksort(a, pivot + 1, end);
+			}
+		}
 
-        // total asymptotic time complexity: O(nlog(n))
-        // total asymptotic space complexity: O(n)
-    }
+		// space complexity: O(n), as quicksort is called a maximum of n times, and swap functions, and pivot variable, are constant in space complexity
+		// time complexity:
+			// worst case: O(n^2), would occur if each time the pivot was at the leftmost of the array, causing the recursive sort graph to be right heavy
+			// average case: O(nlog(n)), would occur if each time the pivot landed at the middle of the array, causuing a balanced graph of height log(n)
 
-    // quicksort implimentation with complexity analysis
-    int partition(int a[], int start, int end) {
-        int pivot = a[end]; // O(1)
-        int pIndex = start; // O(1)
-        for (int i = start; i < end; i++) {
-            if (a[i] > pivot) {
-                swap(a[i], a[pIndex]); // O(1)
-                pIndex++;
-            }
-        } // worst case time complexity: O(n)
-        swap(a[end], a[pIndex]); // O(1)
-        return pIndex;
-    }
+		void merge(int l[], int nl, int r[], int nr, int a[]) {
+			int i = 0;
+			int j = 0;
+			int k = 0;
+			while (i < nl && j < nr) {
+				if (l[i] < r[j]) {
+					a[k] = l[i];
+					i++;
+				}
+				else {
+					a[k] = r[j];
+					j++;
+				}
+				k++;
+			}
+			while (i < nl) {
+				a[k] = l[i];
+				i++;
+				k++;
+			}
+			while (j < nr) {
+				a[k] = r[j];
+				j++;
+				k++;
+			}
+		}
 
-    void quicksort(int a[], int start, int end) {
-        if (start >= end)return;
-        int pivot = partition(a, start, end); // time complexity: O(n)
-        // space complexity: O(1)
-        quicksort(a, start, pivot - 1); // called untill start >= end: worst case, pivot is the largest element: n calls, n - 1 comparisons
-        quicksort(a, pivot + 1, end); // O(n^2)
+		void mergesort(int a[], int len) {
+			if (len >= 2) {
+				int mid = len / 2;
+				int* l = new int[mid]();
+				int* r = new int[len - mid]();
 
-        // total asymptotic time complexity: O(n^2)
-        // total asymptotic space complexity: O(log(n))
-    }
+				for (int i = 0; i < mid; i++) {
+					l[i] = a[i];
+				}
+				for (int i = mid; i < len; i++) {
+					r[i - mid] = a[i];
+				}
+
+				mergesort(l, mid);
+				mergesort(r, len - mid);
+				merge(l, mid, r, len - mid, a);
+			}
+		}
+
+		// space complexity: O(n), as the splitting of the main array into sub arrays are linear in space complexity
+		// time complexity: 
 };
 
-//// section for Object Orientated Principles
-class OOP {
-private:
-    int encap = 1;
-public:
-    int abstract(int e) {
-        encap = e;
-        return encap;
-    }
-};
-
-// inheritance
-class inherit : public OOP {};
-
-// polymorphism
-class poly {
-protected:
-    int x = 0;
-public:
-    poly(int _x = 0) {
-        x = _x;
-    }
-    virtual int prop() {
-        return 0;
-    }
-};
-
-class child0 : public poly {
-public:
-    child0(int _x = 0) : poly(_x) {}
-    int prop() {
-        return x + 1;
-    }
-};
-
-class child1 : public poly {
-public:
-    child1(int _x = 0) : poly(_x){}
-    int prop() {
-        return x * 10;
-    }
-};
-
-//// data structures section
 class linked_list {
-private:
-    struct node {
-        int data;
-        struct node* next;
-    };
-    struct node* head;
-public:
-    linked_list() {
-        head = NULL;
-    }
-    void add(int n) {
-        struct node* nn = new struct node;
-        nn->data = n;
-        nn->next = head;
-        head = nn;
-    }
-    bool search(int n) {
-        struct node* nn = head;
-        while (nn != NULL) {
-            if (nn->data == n)return true;
-            nn = nn->next;
-        }
-        return false;
-    }
+	private:
+		struct node {
+			int data;
+			struct node* next;
+		};
+
+		struct node* head;
+
+	public:
+		linked_list() {
+			head = NULL;
+		}
+
+		void add(int n) {
+			struct node* nn = new struct node;
+			nn->data = n;
+			nn->next = head;
+
+			head = nn;
+		}
+
+		void remove() {
+			struct node* nn = head->next;
+
+			head = nn;
+		}
+
+		bool search(int n) {
+			struct node* nn = head;
+			while (nn != NULL) {
+				if (nn->data = n)return true;
+
+				nn = nn->next;
+			}
+			return false;
+		}
 };
 
 class stack {
-private:
-    struct node {
-        int data;
-        struct node* next;
-    };
-    struct node* head;
-public:
-    stack() {
-        head = NULL;
-    }
-    void push(int n) {
-        struct node* nn = new struct node;
-        nn->data = n;
-        nn->next=head;
-        head = nn;
-    }
-    int pop() {
-        if (head == NULL) {
-            cout << "empty" << endl;
-            return 0;
-        }
-        int popped = head->data;
-        struct node* nn = head->next;
-        head = nn;
-        return popped;
-    }
-    bool search(int n) {
-        int p = pop();
-        while (p != NULL) {
-            if (p == n)return true;
-            p = pop();
-        }
-        return false;
-    }
+	private:
+		struct node {
+			int data;
+			struct node* next;
+		};
+
+		struct node* head;
+
+	public:
+		stack() {
+			head = NULL;
+		}
+
+		void push(int n) {
+			struct node* nn = new struct node;
+			nn->data = n;
+			nn->next = head;
+
+			head = nn;
+		}
+
+		int pop() {
+			int popped = head->data;
+
+			struct node* nn = head->next;
+
+			head = nn;
+
+			return popped;
+		}
+
+		bool search(int n) {
+			struct node* nn = head;
+			while (nn != NULL) {
+				if (nn->data == n)return true;
+
+				nn = nn->next;
+			}
+			return false;
+		}
+};
+
+class OOP {
+	private:
+		int encap = 1;
+
+	public:
+		int abstract(int _x) {
+			encap = _x;
+			return encap;
+		}
+};
+
+class inherit : public OOP{};
+
+class poly {
+	protected:
+		int x = 0;
+
+	public:
+		poly(int _x = 0) {
+			x = _x;
+		}
+		virtual int prop() {
+			return 0;
+		}
+};
+
+class child0 : public poly {
+	public:
+		child0(int _x = 0) : poly(_x){}
+		int prop() {
+			return x + 1;
+		}
+};
+
+class child1 : public poly {
+	public:
+		child1(int _x = 0) : poly(_x){}
+		int prop() {
+			return x << 1;
+		}
 };
 
 void print(int a[], int len) {
-    for (int i = 0; i < len; i++) {
-        cout << a[i] << ' ';
-    }
-    cout << endl;
+	for (int i = 0; i < len; i++) {
+		cout << a[i] << ' ';
+	}
+	cout << endl;
 }
 
 int main() {
-    OOP o;
-    // encapsulate o.encap
-    cout << o.abstract(7) << endl;
+	sort s;
 
-    inherit i;
-    cout << i.abstract(8) << endl;
+	int a0[] = { 12, -2, 34, 7 };
+	int len = sizeof(a0) / sizeof(a0[0]);
+	s.mergesort(a0, len);
+	print(a0, len);
 
-    poly* p;
-    child0 c0(1);
-    child1 c1(1);
-    p = &c0;
-    cout << c0.prop() << endl;
-    p = &c1;
-    cout << c1.prop() << endl;
+	int a1[] = { 12, -2, 34, 7 };
+	s.heapsort(a1, len);
+	print(a1, len);
 
-    linked_list ll;
-    for (int i = 0; i < 10; i++) {
-        ll.add(i);
-    }
-    cout << ll.search(11) << endl;
-    cout << ll.search(2) << endl;
+	int a2[] = { 12, -2, 34, 7 };
+	s.quicksort(a2, 0, len);
+	print(a2, len);
 
-    stack stk;
-    for (int i = 0; i < 4; i++) {
-        stk.push(i);
-    }
-    cout << stk.search(2) << endl;
+	linked_list ll;
 
-    sort s;
-    int a0[] = { 1, -3, 6, -1, 20, 67, 2 };
-    int len = sizeof(a0) / sizeof(a0[0]);
-    s.heapsort(a0, len);
-    print(a0, len);
+	for (int i = 0; i < 6; i++) {
+		ll.add(i);
+	}
+	cout << ll.search(3) << endl;
 
-    int a1[] = { 1, -3, 6, -1, 20, 67, 2 };
-    s.mergesort(a1, len);
-    print(a1, len);
+	stack stk;
 
-    int a2[] = { 1, -3, 6, -1, 20, 67, 2 };
-    s.quicksort(a2, 0, len);
-    print(a2, len);
+	for (int i = 0; i < 6; i++) {
+		stk.push(i);
+	}
+	cout << stk.search(3) << endl;
 
-    return 0;
+
+	OOP o;
+	
+	cout << o.abstract(9) << endl;
+
+	inherit i;
+
+	cout << i.abstract(10) << endl;
+
+	poly* p;
+	child0 c0(1);
+	child1 c1(1);
+
+	p = &c0;
+	cout << p->prop() << endl;
+
+	p = &c1;
+	cout << p->prop() << endl;
+	return 0;
 }
