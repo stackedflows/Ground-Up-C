3dc0757b4e37d23599347f6794779bfda1bda307
Kai <73109076+stackedflows@users.noreply.github.com>
2021-05-24 20:45:11 +0100

    Update graph.cpp

--- fundamentals/graph.cpp
+++ fundamentals/graph.cpp
@@ -1,59 +1,102 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
 class vertex {
 private:
-	int data;
-	bool visited = false;
-	vector<int> neibours;
+	int id;
+	vector<int> neibs;
+	int nneibs;
 public:
-	vertex(int _data, vector<int> _neibours) {
-		data = _data;
-		visited = _visited;
-		neibours = _neibours;
+	bool discovered;
+	vertex(int _id, vector<int> _neibs, int _nneibs) {
+		id = _id;
+		neibs = _neibs;
+		nneibs = _nneibs;
+		discovered = false;
 	}
-	int get_data() {
-		return data;
+	int get_id() {
+		return id;
 	}
-	bool get_visited() {
-		return visited;
+	vector<int> get_neibs() {
+		return neibs;
 	}
-	void set_visited() {
-		visited = true;
-	}
-	vector<int> get_neibours() {
-		return neibours;
+	int get_nneibs() {
+		return nneibs;
 	}
 };
 
 class graph {
 private:
-	vector<vertex> verticies;
+	int count = 0;
 public:
-	void add(vertex v) {
-		verticies.push_back(v);
+	vector<vertex*> vertices;
+	void add(int id, vector<int> neibs, int nneibs) {
+		vertex* v = new vertex(id, neibs, nneibs);
+		vertices.push_back(v);
+		count++;
+	}
+	int get_count() {
+		return count;
 	}
 };
 
+void dfs(graph g, vertex * v) {
+	cout << v->get_id() << ' ';
+	v->discovered = true;
+	int nneibs = v->get_nneibs();
+	vector<int> neibs = v->get_neibs();
+	for (int i = 0; i < nneibs; i++) {
+		int curr = neibs[i];
+		vertex* cv = g.vertices[curr];
+		if (cv->discovered == false) {
+			return dfs(g, cv);
+		}
+	}
+}
+
+void bfs(graph g, vertex* root){
+	queue<vertex*> q;
+	root->discovered = true;
+	q.push(root);
+	while (q.size() > 0) {
+		vertex* v = q.front();
+		cout << v->get_id() << ' ';
+		q.pop();
+		vector<int> neibs = v->get_neibs();
+		for (int i = 0; i < v->get_nneibs(); i++) {
+			vertex* k = g.vertices[neibs[i]];
+			if (k->discovered == false) {
+				k->discovered = true;
+				q.push(k);
+			}
+		}
+	}
+	cout << endl;
+}
+
 int main() {
 	graph g;
-	vertex v0(0, false, { 1 });
-	g.add(v0);
-	vertex v1(1, false, { 0, 5, 4 });
-	g.add(v1);
-	vertex v2(2, false, { 1,3,7,6 });
-	g.add(v2);
-	vertex v3(3, false, { 2,8 });
-	g.add(v3);
-	vertex v4(4, false, { 1 });
-	g.add(v4);
-	vertex v5(5, false, { 1 });
-	g.add(v5);
-	vertex v6(6, false, { 2 });
-	g.add(v6);
-	vertex v7(7, false, { 2 });
-	g.add(v7);
+	g.add(0, { 1, 2 }, 2);
+	g.add(1, { 0, 3, 4 }, 3);
+	g.add(2, { 1, 5, 6 }, 3);
+	g.add(3, { 1 }, 1);
+	g.add(4, { 1, 7 }, 1);
+	g.add(5, { 2 }, 1);
+	g.add(6, { 2 }, 1);
+	g.add(7, { 4 }, 1);
+
+	/*
+	 	for (int i = 0; i < g.get_count(); i++) {
+			if (g.vertices[i]->discovered == false) {
+				dfs(g, g.vertices[i]);
+			}
+		}
+	*/
+
+	bfs(g, g.vertices[0]);
+
 	return 0;
 }
