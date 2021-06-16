c941caba19d1ddfefe786da7552bcc03d1fdc833
Kai <73109076+stackedflows@users.noreply.github.com>
2021-06-16 17:57:04 +0100

    Update FND.cpp

--- fundamentals/FND.cpp
+++ fundamentals/FND.cpp
@@ -573,6 +573,98 @@ public:
 	}
 };
 
+class heapQ {
+public:
+	struct item {
+		int val;
+		int pri;
+	};
+
+	vector<item> heap;
+	// O(logn)
+	void siftUp(int i) {
+		int above = floor((i - 1) / 2);
+		if (above >= 0 and heap[i].pri > heap[above].pri) {
+			swap(heap[i], heap[above]);
+			siftUp(above);
+		}
+	}
+	// O(logn)
+	void siftDown(int i) {
+
+		int largest = i;
+		int left = 2 * i + 1;
+		int right = 2 * i + 2;
+		if (left < heap.size() and heap[largest].pri < heap[left].pri) {
+			largest = left;
+		}
+		if (right < heap.size() and heap[largest].pri < heap[right].pri) {
+			largest = right;
+		}
+		if (largest != i) {
+			swap(heap[largest], heap[i]);
+			siftDown(largest);
+		}
+	}
+public:
+	void push(item i) {
+		heap.push_back(i);
+		siftUp(heap.size() - 1);
+	}
+	int pop() {
+		item head = heap.front();
+		item back = heap.back();
+		heap.front() = back;
+		heap.pop_back();
+		siftDown(0);
+		return head.val;
+	}
+	void disp() {
+		for (int i = 0; i < heap.size(); i++) {
+			cout << heap[i].val << ':' << heap[i].pri << ' ';
+		}cout << endl;
+	}
+	heapQ() {
+		int a[] = { 1, 2, 8, 3, 0, 2, 6, 2, 6, 1, 5, 3, 5, 1 };
+		int len = sizeof(a) / sizeof(a[0]);
+		// top frequencies 1,2,3,6,5,8,0
+		vector<item> freqs_;
+		for (int i = 0; i < len; i++) {
+			item it = { 0, -1 };
+			freqs_.push_back(it);
+		}
+		for (int i = 0; i < freqs_.size(); i++) {
+			if (freqs_[a[i]].pri == -1) {
+				freqs_[a[i]].val = a[i];
+				freqs_[a[i]].pri = 1;
+			}
+			else {
+				freqs_[a[i]].pri++;
+			}
+		}
+		vector<item> heapIn;
+		for (int i = 0; i < freqs_.size(); i++) {
+			if (freqs_[i].pri == -1) {
+				continue;
+			}
+			else {
+				heapIn.push_back(freqs_[i]);
+			}
+		}
+
+
+		for (int i = 0; i < heapIn.size(); i++) {
+			push(heapIn[i]);
+		}
+		disp();
+		while (heap.size() > 0) {
+			cout << pop() << endl;
+		}
+		disp();
+	}
+};
+
+
 int main() {
 	graph g;
 	recursion r;
@@ -582,5 +674,6 @@ int main() {
 	hashMap hm;
 	stack stk;
 	Queue q;
+	heapQ hq;
 	return 0;
 }
